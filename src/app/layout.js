b6a21182a5e70e332d8723b5fc8d5adc84c6922b
wayu <iarahtuyaw@gmail.com>
2023-08-08 09:01:09 +0700

    finish

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,11 +26,7 @@ export default function RootLayout({ children }) {
           <Header />
           <Navbar />
           {children}
-          <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
-          />
+          <Footer year="2023" fullName="Wayu Tharai" studentId="650610806" />
         </div>
       </body>
     </html>
